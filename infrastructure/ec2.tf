96d844b62a5ce532669e0b9824c2873c3dc4c9bd
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-02-24 23:20:06 +0000

    Removing single instance.

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -10,12 +10,3 @@ module "sg_http" {
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-# Instances
-// module "instance" {
-//   source = "./modules/instance"
-//   vpc_id = module.vpc.vpc_id
-//   subnet_id = module.vpc.private_subnets[0]
-//   security_group = module.sg_http.this_security_group_id
-//   tags = var.tags
-// }
