fd77e384baab09c762ea2643c3825c39e25f79b3
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-02 21:21:28 +0000

    Adding ELB DNS name in output.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,3 +3,8 @@ output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = module.vpc.public_subnets
 }
+
+output "elb_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = aws_lb.http.dns_name
+}
