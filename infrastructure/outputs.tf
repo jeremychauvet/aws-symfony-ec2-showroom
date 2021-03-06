b7c1312438b412e35ecfcd0a69000ab8cd10ecc3
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-06 23:04:15 +0000

    Adding route table and nat gateway.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -8,3 +8,13 @@ output "route53_dns_name" {
   description = "The DNS public name."
   value       = aws_route53_record.www.name
 }
+
+output "private_route_table_aza" {
+  description = "Private route table AZa"
+  value       = module.vpc.private_route_table_ids[0]
+}
+
+output "natgw_ids_aza" {
+  description = "NAT gateway AZa"
+  value       = module.vpc.natgw_ids[0]
+}
