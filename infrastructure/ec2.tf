21bfd7689712aba739f44d32e48ae1ef92b38652
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-06 23:03:43 +0000

    Adding key and SG.

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -9,18 +9,23 @@ module "sg_http" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  computed_ingress_rules           = ["ssh-tcp", "http-80-tcp"]
+  number_of_computed_ingress_rules = 2
 }
 
 resource "aws_launch_template" "symfony" {
   image_id      = var.ami_id
   instance_type = var.instance_type
+  key_name      = "symfony"
 
   instance_market_options {
     market_type = "spot"
   }
 
   network_interfaces {
-    associate_public_ip_address = false
+    #checkov:skip=CKV_AWS_88:EC2 instance should not have public IP
+    associate_public_ip_address = true
     security_groups             = [module.sg_http.this_security_group_id]
   }
 
