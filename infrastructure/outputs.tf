88e9c798666dd8b61af44a59088a4246c75693a4
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-06 21:53:36 +0000

    Adding DNS name in output.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,10 +1,10 @@
 # VPC
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
 output "elb_dns_name" {
   description = "The DNS name of the load balancer."
   value       = aws_lb.http.dns_name
+}
+
+output "route53_dns_name" {
+  description = "The DNS public name."
+  value       = aws_route53_record.www.name
 }
