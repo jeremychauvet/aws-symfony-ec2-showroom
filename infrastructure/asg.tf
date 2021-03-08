0c444f2f760efee78124f650947a44cf665e9237
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-03-08 20:46:48 +0000

    Disable AZb

--- infrastructure/asg.tf
+++ infrastructure/asg.tf
@@ -35,32 +35,32 @@ resource "aws_autoscaling_attachment" "asg_attachment_alb_aza" {
 }
 
 # AZb.
-resource "aws_autoscaling_group" "azb" {
-  name                 = "dev.azb.aws_autoscaling_group"
-  max_size             = 2
-  min_size             = 1
-  force_delete         = true
-  placement_group      = aws_placement_group.cluster.id
-  termination_policies = ["OldestInstance"]
+// resource "aws_autoscaling_group" "azb" {
+//   name                 = "dev.azb.aws_autoscaling_group"
+//   max_size             = 2
+//   min_size             = 1
+//   force_delete         = true
+//   placement_group      = aws_placement_group.cluster.id
+//   termination_policies = ["OldestInstance"]
 
-  launch_template {
-    id      = aws_launch_template.symfony.id
-    version = "$Latest"
-  }
+//   launch_template {
+//     id      = aws_launch_template.symfony.id
+//     version = "$Latest"
+//   }
 
-  // Launch instances in private subnet of AZa.
-  vpc_zone_identifier = [module.vpc.private_subnets[1]]
+//   // Launch instances in private subnet of AZa.
+//   vpc_zone_identifier = [module.vpc.private_subnets[1]]
 
-  timeouts {
-    delete = "5m"
-  }
+//   timeouts {
+//     delete = "5m"
+//   }
 
-  lifecycle {
-    ignore_changes = [load_balancers, target_group_arns]
-  }
-}
+//   lifecycle {
+//     ignore_changes = [load_balancers, target_group_arns]
+//   }
+// }
 
-resource "aws_autoscaling_attachment" "asg_attachment_alb_azb" {
-  autoscaling_group_name = aws_autoscaling_group.azb.id
-  alb_target_group_arn   = aws_lb_target_group.azb.arn
-}
+// resource "aws_autoscaling_attachment" "asg_attachment_alb_azb" {
+//   autoscaling_group_name = aws_autoscaling_group.azb.id
+//   alb_target_group_arn   = aws_lb_target_group.azb.arn
+// }
