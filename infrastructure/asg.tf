ef39b794d38156e3142624ebd9d9fea7acb7f8a9
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-04-23 10:10:08 +0200

    Activate ASG AZa.

--- infrastructure/asg.tf
+++ infrastructure/asg.tf
@@ -4,37 +4,34 @@ resource "aws_placement_group" "cluster" {
 }
 
 # AZa.
-// resource "aws_autoscaling_group" "aza" {
-//   name                 = "dev.aza.aws_autoscaling_group"
-//   max_size             = 4
-//   min_size             = 1
-//   force_delete         = true
-//   placement_group      = aws_placement_group.cluster.id
-//   termination_policies = ["OldestInstance"]
+resource "aws_autoscaling_group" "aza" {
+  name                 = "dev.aza.aws_autoscaling_group"
+  max_size             = 1
+  desired_capacity     = 0
+  min_size             = 0
+  force_delete         = true
+  placement_group      = aws_placement_group.cluster.id
+  termination_policies = ["OldestInstance"]
 
-//   launch_template {
-//     id      = aws_launch_template.symfony.id
-//     version = "$Latest"
-//   }
+  launch_configuration    = aws_launch_configuration.symfony.id
+  service_linked_role_arn = aws_iam_service_linked_role.asg.arn
 
-//   service_linked_role_arn = aws_iam_service_linked_role.asg.arn
+  // Launch instances in private subnet of AZa.
+  vpc_zone_identifier = [module.vpc.private_subnets[0]]
 
-//   // Launch instances in private subnet of AZa.
-//   vpc_zone_identifier = [module.vpc.private_subnets[0]]
+  timeouts {
+    delete = "5m"
+  }
 
-//   timeouts {
-//     delete = "5m"
-//   }
+  lifecycle {
+    ignore_changes = [load_balancers, target_group_arns]
+  }
+}
 
-//   lifecycle {
-//     ignore_changes = [load_balancers, target_group_arns]
-//   }
-// }
-
-// resource "aws_autoscaling_attachment" "asg_attachment_alb_aza" {
-//   autoscaling_group_name = aws_autoscaling_group.aza.id
-//   alb_target_group_arn   = aws_lb_target_group.aza.arn
-// }
+resource "aws_autoscaling_attachment" "asg_attachment_alb_aza" {
+  autoscaling_group_name = aws_autoscaling_group.aza.id
+  alb_target_group_arn   = aws_lb_target_group.aza.arn
+}
 
 # AZb.
 // resource "aws_autoscaling_group" "azb" {
