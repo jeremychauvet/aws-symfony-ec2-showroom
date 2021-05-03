347f6a5cb6b001d4e7d26a9b5590273062d12e2f
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-05-03 15:30:14 +0200

    Adding Route53 record for bastion.

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -14,3 +14,12 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
   }
 }
+
+# Bastion.
+resource "aws_route53_record" "bastion" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "bastion.${var.route53_zone}"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.bastion.public_ip]
+}
