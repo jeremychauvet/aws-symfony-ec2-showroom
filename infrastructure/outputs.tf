9640e2c402b33ab0850d23075c06a6112893f808
Jérémy Chauvet <jer.chauvet@gmail.com>
2021-05-03 17:22:17 +0200

    Clean outputs.

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,16 +9,6 @@ output "route53_dns_name" {
   value       = aws_route53_record.www.name
 }
 
-output "private_route_table_aza" {
-  description = "Private route table AZa"
-  value       = module.vpc.private_route_table_ids[0]
-}
-
-output "natgw_ids_aza" {
-  description = "NAT gateway AZa"
-  value       = module.vpc.natgw_ids[0]
-}
-
 output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
