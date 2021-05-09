11a6c528797733df81628f32400530780fdc1ccf
Jeremy Chauvet <jer.chauvet@gmail.com>
2021-05-09 19:18:37 +0200

    Adding AZb and tags.

--- infrastructure/asg.tf
+++ infrastructure/asg.tf
@@ -7,7 +7,6 @@ resource "aws_placement_group" "cluster" {
 resource "aws_autoscaling_group" "aza" {
   name                 = "dev.aza.aws_autoscaling_group"
   max_size             = 1
-  desired_capacity     = 0
   min_size             = 0
   force_delete         = true
   placement_group      = aws_placement_group.cluster.id
@@ -26,6 +25,11 @@ resource "aws_autoscaling_group" "aza" {
   lifecycle {
     ignore_changes = [load_balancers, target_group_arns]
   }
+
+  tags = [merge(var.tags, {
+    IsAvailableForChaosMonkey = "true"
+    AvailabilityZone          = "a"
+  })]
 }
 
 resource "aws_autoscaling_attachment" "asg_attachment_alb_aza" {
@@ -34,32 +38,35 @@ resource "aws_autoscaling_attachment" "asg_attachment_alb_aza" {
 }
 
 # AZb.
-// resource "aws_autoscaling_group" "azb" {
-//   name                 = "dev.azb.aws_autoscaling_group"
-//   max_size             = 2
-//   min_size             = 1
-//   force_delete         = true
-//   placement_group      = aws_placement_group.cluster.id
-//   termination_policies = ["OldestInstance"]
+resource "aws_autoscaling_group" "azb" {
+  name                 = "dev.azb.aws_autoscaling_group"
+  max_size             = 1
+  min_size             = 0
+  force_delete         = true
+  placement_group      = aws_placement_group.cluster.id
+  termination_policies = ["OldestInstance"]
 
-//   launch_template {
-//     id      = aws_launch_template.symfony.id
-//     version = "$Latest"
-//   }
+  launch_configuration    = aws_launch_configuration.symfony.id
+  service_linked_role_arn = aws_iam_service_linked_role.asg.arn
 
-//   // Launch instances in private subnet of AZa.
-//   vpc_zone_identifier = [module.vpc.private_subnets[1]]
+  // Launch instances in private subnet of AZa.
+  vpc_zone_identifier = [module.vpc.private_subnets[1]]
 
-//   timeouts {
-//     delete = "5m"
-//   }
+  timeouts {
+    delete = "5m"
+  }
 
-//   lifecycle {
-//     ignore_changes = [load_balancers, target_group_arns]
-//   }
-// }
+  lifecycle {
+    ignore_changes = [load_balancers, target_group_arns]
+  }
 
-// resource "aws_autoscaling_attachment" "asg_attachment_alb_azb" {
-//   autoscaling_group_name = aws_autoscaling_group.azb.id
-//   alb_target_group_arn   = aws_lb_target_group.azb.arn
-// }
+  tags = [merge(var.tags, {
+    IsAvailableForChaosMonkey = "true"
+    AvailabilityZone          = "b"
+  })]
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_alb_azb" {
+  autoscaling_group_name = aws_autoscaling_group.azb.id
+  alb_target_group_arn   = aws_lb_target_group.azb.arn
+}
